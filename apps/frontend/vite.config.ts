5bb54a5d41e2636a4f0fa037737bb414afcd6b98
madeandenis <123639454+madeandenis@users.noreply.github.com>
2024-07-09 10:13:55 +0300

    init(frontend,backend) from cli

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/vibeSync',
+  cacheDir: '../../node_modules/.vite/apps/frontend',
 
   server: {
     port: 4200,
@@ -25,7 +25,7 @@ export default defineConfig({
   // },
 
   build: {
-    outDir: '../../dist/apps/vibeSync',
+    outDir: '../../dist/apps/frontend',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
